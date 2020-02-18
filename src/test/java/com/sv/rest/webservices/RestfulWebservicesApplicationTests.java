f85c22da31ed566c02f23be4aebbe439ad9433dd
Sumitha Varghese <sumitha.var@gmail.com>
2020-02-17 22:39:28 -0500

    first rest api

---
+++ src/test/java/com/sv/rest/webservices/RestfulWebservicesApplicationTests.java
@@ -0,0 +1,13 @@
+package com.sv.rest.webservices;
+
+import org.junit.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class RestfulWebservicesApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
