378700dd0d02a9fe17e6209be06ca457d5e3ed93
Sumitha Varghese <sumitha.var@gmail.com>
2020-02-17 21:02:38 -0500

    Initial commit

---
+++ src/main/java/com/sv/rest/webservices/DemoApplication.java
@@ -0,0 +1,13 @@
+package com.sv.rest.webservices;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DemoApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DemoApplication.class, args);
+	}
+
+}
